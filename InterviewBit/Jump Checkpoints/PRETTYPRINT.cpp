033df2759a73fc542ad3e911489d56551462ac70
Rajan Pandey <rajanpandey2508@gmail.com>
2019-06-21 23:51:27 +0530

    https://www.interviewbit.com/problems/prettyprint/

--- InterviewBit/Jump Checkpoints/PRETTYPRINT.cpp
+++ InterviewBit/Jump Checkpoints/PRETTYPRINT.cpp
@@ -1,22 +1,20 @@
 vector<vector<int> > Solution::prettyPrint(int A) {
-    int size, temp, limit, i, j;
+    int size = 2*A-1;
+    int left = 0;
+    int right = size;
+    
+    vector<vector<int>> ans(size, vector<int>(size));
 
-    size = 2*A-1;
-    limit = size;
-    temp = 0;
-
-    vector<vector<int> > mat(size, vector<int>(size));
-
-    while(A > 0){
-        for(i = temp; i < limit; i++){
-            for(j = temp; j < limit; j++){
-                mat[i][j] = A;
+    while(A>0){
+        for(int i=left; i<right; i++) {
+            for(int j=left; j<right; j++) {
+                ans[i][j] = A;
             }
         }
         A--;
-        limit--;
-        temp++;
+        left++;
+        right--;
     }
 
-    return mat;
+    return ans;
 }
