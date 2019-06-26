320f6fdd17c5d9d5fb21925f5b12595184febba4
Rajan Pandey <rajanpandey2508@gmail.com>
2019-06-26 16:32:36 +0530

    https://www.interviewbit.com/problems/search-for-a-range/

---
+++ InterviewBit/Binary Search/Search for a Range.cpp
@@ -0,0 +1,30 @@
+int binary_search(const vector<int> &arr, int element, bool searchFirst) { 
+    int L = 0, R = arr.size()-1, result = -1;
+    while (L <= R) { 
+        int mid = L+(R-L)/2; 
+        
+        if (arr[mid] == element) {
+            result = mid;
+            if(searchFirst) {
+                R = mid-1;
+            } else {
+                L = mid+1;
+            }
+        } else if (arr[mid] > element) {
+            R = mid-1;
+        } else {
+            L = mid+1; 
+        }
+    }
+    return result;
+} 
+
+vector<int> Solution::searchRange(const vector<int> &A, int B) {
+    vector<int> ans;
+    
+    ans.push_back(binary_search(A, B, true));
+    ans.push_back(binary_search(A, B, false));
+    
+    return ans;
+
+}
