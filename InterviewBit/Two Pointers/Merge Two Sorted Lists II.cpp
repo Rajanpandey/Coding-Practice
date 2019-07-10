2fb02829aa191597532e2dc74733afcf1914e07e
Rajan Pandey <rajanpandey2508@gmail.com>
2019-07-10 18:46:26 +0530

    https://www.interviewbit.com/problems/merge-two-sorted-lists-ii/

---
+++ InterviewBit/Two Pointers/Merge Two Sorted Lists II.cpp
@@ -0,0 +1,19 @@
+void Solution::merge(vector<int> &A, vector<int> &B) {
+    int i = 0, j = 0;
+    vector<int> ans;
+    
+    while((i < A.size()) && (j < B.size())){
+        if(A[i] > B[j]){
+            A.insert(A.begin()+i, B[j]);
+            i++;
+            j++;
+        }
+        else{
+            i++;
+        }
+    }
+    while(j != B.size()){
+        A.push_back(B[j]);
+        j++;
+    }
+}
