7e5c142cd5c7b1663d7208ae22a54da438b3e0fb
Rajan Pandey <rajanpandey2508@gmail.com>
2019-07-11 00:45:59 +0530

    https://www.interviewbit.com/problems/intersection-of-linked-lists/

---
+++ InterviewBit/Linked Lists/Intersection of Linked Lists.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ 
+ListNode* Solution::getIntersectionNode(ListNode* A, ListNode* B) {
+    if(A == NULL || B == NULL) {
+        return NULL;
+    }
+    
+    ListNode *first = A;
+    ListNode *second = B;
+    while(first != second) {
+        if(first == NULL){
+          first = B;
+      } else {
+          first = first -> next;
+      }
+      if(second == NULL) {
+          second = A;
+      } else {
+          second = second -> next;
+      }
+    }
+    return first;
+}
