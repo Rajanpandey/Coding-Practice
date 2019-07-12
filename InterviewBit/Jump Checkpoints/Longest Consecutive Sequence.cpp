88b9769c9e3154933d0d1472e635cf5e9eeec407
Rajan Pandey <rajanpandey2508@gmail.com>
2019-07-12 12:37:09 +0530

    https://www.interviewbit.com/problems/longest-consecutive-sequence/

--- InterviewBit/Jump Checkpoints/Longest Consecutive Sequence.cpp
+++ InterviewBit/Jump Checkpoints/Longest Consecutive Sequence.cpp
@@ -1,17 +1,15 @@
 int Solution::longestConsecutive(const vector<int> &A) {
+    int ans = INT_MIN;
     unordered_set<int> hash;
+    
     for(int i=0; i<A.size(); i++){
         hash.insert(A[i]);
     }
     
-    int count=1;
-    int ans=INT_MIN;
-    
     for(int i=0; i<A.size(); i++){
-        int j=1;
-        count=1;
-        if(hash.find(A[i]-1) == hash.end()){
-            while(hash.find(A[i]+j) != hash.end()){
+        int j = 1, count = 1;
+        if(hash.find(A[i]-1) == hash.end()) {
+            while(hash.find(A[i]+j) != hash.end()) {
                 j++;
                 count++;
             }
