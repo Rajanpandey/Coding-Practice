18e9c6d6d9c135339e1a91d2942e1d155f548227
Rajan Pandey <rajanpandey2508@gmail.com>
2019-07-14 17:31:48 +0530

    https://www.interviewbit.com/problems/identical-binary-trees/

--- InterviewBit/Trees/Identical Binary Trees.cpp
+++ InterviewBit/Trees/Identical Binary Trees.cpp
@@ -8,12 +8,8 @@
  * };
  */
 int Solution::isSameTree(TreeNode* A, TreeNode* B) {
-    if(A == NULL && B == NULL) {
-        return 1;
-    } else if ((A == NULL && B != NULL) || (A != NULL && B == NULL)) {
-        return 0;
-    } else if(A->val == B->val) {
-        return min(isSameTree(A->left, B->left), isSameTree(A->right, B->right));
+    if(A == NULL || B == NULL) {
+        return A == B;
     }
-    return 0;
+    return (A->val == B->val) && isSameTree(A->left, B->left) && isSameTree(A->right, B->right);
 }
