a313d1d5bd52249c6cceb2bc7ee61655d1fc17bf
Rajan Pandey <rajanpandey2508@gmail.com>
2019-07-15 18:46:59 +0530

    https://www.interviewbit.com/problems/stairs/

---
+++ InterviewBit/Dynamic Programming/Stairs.cpp
@@ -0,0 +1,12 @@
+int Solution::climbStairs(int A) {
+    int dp[A+4];
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+    
+    for(int i=3; i<=A; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    
+    return dp[A];
+}
