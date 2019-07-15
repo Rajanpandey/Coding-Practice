d21f4a0ef42df3d979484550f80e39acc949b884
Rajan Pandey <rajanpandey2508@gmail.com>
2019-07-15 23:18:04 +0530

    https://www.interviewbit.com/problems/jump-game-array/

--- InterviewBit/Dynamic Programming/Jump Game Array.cpp
+++ InterviewBit/Dynamic Programming/Jump Game Array.cpp
@@ -1,12 +1,11 @@
 int Solution::canJump(vector<int> &A) {
-    int N = A.size();
     int reach = 0;
-    for(int i=0; i<N; i++){
+    for(int i=0; i<A.size(); i++){
         if(i <= reach){
             reach = max(reach, i+A[i]);
         } else {
             break;
         }
     }
-    return reach >= N-1;
+    return reach >= A.size()-1;
 }
