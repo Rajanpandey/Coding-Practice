f0bd520889f4b7d2e83cded4cb60479d4cb96fe2
Rajan Pandey <rajanpandey2508@gmail.com>
2019-07-18 00:52:19 +0530

    https://www.interviewbit.com/problems/min-sum-path-in-triangle/

---
+++ InterviewBit/Dynamic Programming/Min Sum Path in Triangle.cpp
@@ -0,0 +1,14 @@
+int Solution::minimumdp(vector<vector<int> > &A) {
+    int dp[A.size()];
+    int l = A.size() - 1;
+  
+    for(int i=0; i<A[l].size(); i++) {
+        dp[i] = A[l][i];
+    }
+    for(int i=A.size()-2; i>=0; i--) {
+        for(int j=0; <A[i+1].size()-1; j++) {
+            dp[j] = A[i][j] + min(dp[j], dp[j+1]);
+        }
+    }
+    return dp[0];
+}
