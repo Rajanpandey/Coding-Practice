dea2451a01bf303d2232317e2684fee7a24cf6de
Rajan Pandey <rajanpandey2508@gmail.com>
2019-07-21 16:57:34 +0530

    https://www.interviewbit.com/problems/max-non-negative-subarray/

---
+++ InterviewBit/Arrays/Max Non Negative SubArray.cpp
@@ -0,0 +1,21 @@
+vector<int> Solution::maxset(vector<int> &A) {
+    int x=0, y=0, maxX=0, maxY=0;
+    long long sum=0, maxSum=0;
+
+    for(int i=0; i<A.size(); i++) {
+        if(A[i] >= 0) {
+            sum += A[i];
+            y++;
+            if(sum>maxSum || maxSum>=sum && maxX==x) {
+                maxSum=sum, maxX=x, maxY=y;
+            }
+        } else {
+            x = i+1;
+            y = i+1;
+            sum = 0;
+        }
+    }
+    
+    vector<int> ans(A.begin()+maxX, A.begin()+maxY);
+    return ans;
+}
