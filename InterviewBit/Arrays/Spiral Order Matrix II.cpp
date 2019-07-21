e74bfe97bc23de6fd0e9a4a71b480caeed087ba8
Rajan Pandey <rajanpandey2508@gmail.com>
2019-07-21 12:36:24 +0530

    https://www.interviewbit.com/problems/spiral-order-matrix-ii/

--- InterviewBit/Arrays/Spiral Order Matrix II.cpp
+++ InterviewBit/Arrays/Spiral Order Matrix II.cpp
@@ -1,27 +1,28 @@
 vector<vector<int> > Solution::generateMatrix(int A) {
     vector<vector<int>> ans(A, vector<int>(A, 0));
-    int i = 0, j, k = 1;
+    int i = 0;
+    int j, k = 1;
     
     while(k <= A*A) {
-        //1. horizonal, left to right
+        //left to right
         j = i;      
         while(j < A-i) {
             ans[i][j++] = k++;
         }     
 
-        //2. vertical, top to bottom   
+        //top to bottom   
         j = i+1;
         while(j < A-i) {
             ans[j++][A-i-1] = k++;
         }
         
-        //3. horizonal, right to left 
+        //right to left 
         j = A-i-2;
         while(j > i) {
             ans[A-i-1][j--] = k++;
         }
         
-        //4. vertical, bottom to  top 
+        //bottom to  top 
         j = A-i-1;
         while(j > i) {
             ans[j--][i] = k++;
