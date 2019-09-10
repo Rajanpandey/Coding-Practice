eab8d1d74ecd8f0212da2f8a9db8d3fa89c5385c
Rajan Pandey <rajanpandey2508@gmail.com>
2019-09-10 18:02:48 +0530

    https://codingcompetitions.withgoogle.com/codejam/round/0000000000051705/0000000000088231

---
+++ Google Code Jam 2019/Qualification Round 2019/Forgone Solution.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+main () {
+    int T;
+    cin >> T;
+    for(int k=1; k<=T; k++) {
+        string N;
+        string ans = "";
+        
+        cin >> N;
+        for(int i=0; i<N.size(); i++) {
+            if(N[i] == '4') {
+                N[i] = '3';
+                ans += '1';
+            } else {
+                ans += '0';
+            }
+        }
+        cout << "case #" << k << ": " << N << " ";
+        
+        bool trimmed = false;
+        for(int i=0; i<ans.size(); i++) {
+            if(ans[i] == '0' && !trimmed) {
+                continue;
+            } else {
+                trimmed = true;
+                cout << ans[i];
+            }
+        }
+        cout << endl;
+    }
+}
