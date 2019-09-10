89202050bea5bf1ff930372915c6e0225b2a0785
Rajan Pandey <rajanpandey2508@gmail.com>
2019-09-10 19:25:54 +0530

    https://codingcompetitions.withgoogle.com/codejam/round/0000000000051705/0000000000088231

--- Google Code Jam 2019/Qualification Round 2019/Forgone Solution.rb
+++ Google Code Jam 2019/Qualification Round 2019/Forgone Solution.rb
@@ -2,14 +2,9 @@ T = gets.chomp
 (1..T.to_i).each do |k|
     N = gets.chomp
     ans = ""
-    
     N.each_char.with_index { |char, index|
-        if N[index] == '4'
-            N[index] = '3'
-            ans += '1'
-        else
-            ans += '0'
-        end
+        N[index] = '3' and ans += '1' if char == '4'
+        ans += '0' if char != '4'
     }
-    puts "case ##{k}: #{N} #{ans.sub(/^0/, '')}"
+    puts "case ##{k}: #{N} #{ans.to_i}"
 end
