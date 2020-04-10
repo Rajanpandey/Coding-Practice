44f3d65238bb2aaf2cffef2782c87bc2b26b709b
Rajan Pandey <rajanpandey2508@gmail.com>
2020-04-10 22:47:39 +0530

    https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3286/

---
+++ 30-Day LeetCoding Challenge/Week 1/Move Zeroes.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        for(int slow = 0, fast = 0; fast < nums.size(); fast++) {
+            if(nums[fast] != 0) { 
+                swap(nums[slow++], nums[fast]); 
+            }
+        }
+    }
+};
