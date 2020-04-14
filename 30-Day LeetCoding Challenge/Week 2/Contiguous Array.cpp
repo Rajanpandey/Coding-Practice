e3402407b31a2c9ab47e07be0fdb718f1d8165c7
Rajan Pandey <rajanpandey2508@gmail.com>
2020-04-14 22:39:11 +0530

    https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/529/week-2/3298/

---
+++ 30-Day LeetCoding Challenge/Week 2/Contiguous Array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        map<int, int> table = { {0, -1} };
+        int ans = 0, partial_sum = 0; 
+        
+        for(int i=0; i<nums.size(); i++) {
+            partial_sum += nums[i] ? 1 : -1;
+            table.count(partial_sum) ? ans = max(ans, i - table[partial_sum]) : table[partial_sum] = i;
+        }
+        
+        return ans;
+    }
+};
