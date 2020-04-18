8805b7ff0f1692d300e4645b981c642baec884d7
Rajan Pandey <rajanpandey2508@gmail.com>
2020-04-18 23:46:25 +0530

    https://www.interviewbit.com/problems/repeat-and-missing-number-array/

--- InterviewBit/Arrays/Repeat and Missing Number Array.cpp
+++ InterviewBit/Arrays/Repeat and Missing Number Array.cpp
@@ -1,21 +1,16 @@
 vector<int> Solution::repeatedNumber(const vector<int> &A) {
-    vector<int> ans;
-    long long N = A.size();
     int repeat = 0, missing = 0;
+    long long N = A.size();
     
-    long long sum = (N*(N+1))/2;
-    long long sumSq = (N*(N+1)*(2*N+1))/6;
+    long long sum = (N * (N+1)) / 2;
+    long long sumSq = (N * (N+1) * (2*N+1)) / 6;
     
-    for(int i=0; i<N; i++) {
-        sum -= (long long)A[i];
-        sumSq -= (long long)A[i] * (long long)A[i];
+    for(auto num : A) {
+        sum -= (long long)num;
+        sumSq -= (long long)num * (long long)num;
     }
     
-    missing = (sum + sumSq/sum)/2;
-    repeat = missing-sum;
-    
-    ans.push_back(repeat);
-    ans.push_back(missing);
-    
-    return ans;
+    missing = (sum + sumSq/sum) / 2;
+    repeat = missing - sum;
+    return {repeat, missing};
 }
