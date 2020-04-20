4266a7b47941b9ad9df4b81b3f328569480f852a
Rajan Pandey <rajanpandey2508@gmail.com>
2020-04-20 22:18:46 +0530

    https://www.interviewbit.com/problems/binary-representation/

--- InterviewBit/Maths/Binary Representation.cpp
+++ InterviewBit/Maths/Binary Representation.cpp
@@ -1,10 +1,9 @@
 string Solution::findDigitsInBinary(int A) {
-    string ans;
-    if (A == 0) return "0";
-    while(A>0) {
-        int rem = A%2;
-        ans.push_back((char)('0' + rem));
-        A/=2;
+    string ans = "";
+    if(A == 0) { return "0"; }
+    while(A > 0) {
+        ans += (char)('0' + A % 2);
+        A /= 2;
     }
     reverse(ans.begin(), ans.end()); 
     return ans;
