a2f1a338602bcf3a9795b8e988e4039a3f6ad589
Rajan Pandey <rajanpandey2508@gmail.com>
2020-04-20 23:59:43 +0530

    https://www.interviewbit.com/problems/grid-unique-paths/

--- InterviewBit/Dynamic Programming/Grid Unique Paths.cpp
+++ InterviewBit/Dynamic Programming/Grid Unique Paths.cpp
@@ -1,5 +1,5 @@
 int Solution::uniquePaths(int A, int B) {
-    // m+n-2 C n-1 = (m+n-2)! / (n-1)! (m-1)! 
+    // A+B-2 C B-1 = (A+B-2)! / (A-1)! (B-1)! 
     long long ans = 1;
     for(int i= B; i<(A+B-1); i++) {
         ans *= i;
