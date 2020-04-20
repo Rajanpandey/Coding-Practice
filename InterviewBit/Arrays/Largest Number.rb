280e5b59ec0bd9236c28df536650d022070ccd7f
Rajan Pandey <rajanpandey2508@gmail.com>
2020-04-20 22:44:37 +0530

    https://www.interviewbit.com/problems/largest-number/

--- InterviewBit/Arrays/Largest Number.rb
+++ InterviewBit/Arrays/Largest Number.rb
@@ -2,7 +2,6 @@ class Solution
     # @param a : constant array of integers
     # @return a string
     def largestNumber(a)
-        res = a.map(&:to_s).sort! { |a, b| b + a <=> a + b }.join
-        res[0] == '0' ? '0' : res
+        (res = a.map(&:to_s).sort! { |a, b| b + a <=> a + b }.join)[0] == '0' ? '0' : res
     end
 end
