587ef2918180c3ed0541d10abca96322381f4aaa
Rajan Pandey <rajanpandey2508@gmail.com>
2020-04-20 12:50:52 +0530

    https://www.interviewbit.com/problems/n3-repeat-number/

---
+++ InterviewBit/Arrays/Nby3 Repeat Number.cpp
@@ -0,0 +1,40 @@
+int Solution::repeatedNumber(const vector<int> &A) {
+    int N = A.size();
+    int count1 = 0, count2 = 0, first = 0, second = 0;
+
+    if(N/3 == 0) { return A[0]; }
+
+    for(int i=0; i<N; i++) {
+        if(A[i] == first) {
+            count1++;
+        }
+        else if(A[i] == second) {
+            count2++;
+        }
+
+        else if(count1 == 0) {
+            count1 = 1;
+            first = A[i];
+        }
+        else if(count2 == 0) {
+            count2 = 1;
+            second = A[i];
+        }
+
+        else {
+            count1--;
+            count2--;
+        }
+    }
+
+    count1 = 0;
+    count2 = 0;
+    for(int i=0; i<N; i++) {
+        if(A[i] == first) { count1++; }
+        if(A[i] == second) { count2++; }
+    }
+
+    if(count1 > N/3) { return first; }
+    else if(count2 > N/3) { return second; }
+    return -1;
+}
