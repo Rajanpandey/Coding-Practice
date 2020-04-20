5a810b04b3ff712575cc0500ea5967bd8259919f
Rajan Pandey <rajanpandey2508@gmail.com>
2020-04-20 23:25:49 +0530

    https://www.interviewbit.com/problems/reverse-integer/

--- InterviewBit/Maths/Reverse integer.cpp
+++ InterviewBit/Maths/Reverse integer.cpp
@@ -1,12 +1,8 @@
 int Solution::reverse(int A) {
-    long int sol = 0;
-    
-    while(A != 0) {
+    long sol = 0;
+    while(A) {
         sol = sol*10 + A%10;    
         A = A/10;
     }
-    
-    if(sol > INT_MAX || sol < INT_MIN) { return 0; }
-    
-    return sol;
+    return (sol > INT_MAX || sol < INT_MIN) ? 0 : rev;
 }
