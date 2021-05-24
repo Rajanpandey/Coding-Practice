f3e9f57a24becdd939df07721ff397c1b1357eea
Rajan Pandey <rajanpandey2508@gmail.com>
2021-05-24 17:33:47 +0530

    https://leetcode.com/problems/rotate-array/

--- LeetCode/189. Rotate Array.cpp
+++ LeetCode/189. Rotate Array.cpp
@@ -14,3 +14,16 @@ public:
         nums = newArr;
     }
 };
+
+/*
+O(1) Space Complexity solution:
+
+    void rotate(vector<int>& nums, int k) {
+        int N = nums.size();
+
+        reverse(nums, nums + N - k);
+        reverse(nums + N - k, nums + N);
+        reverse(nums, nums + N);
+    }
+
+*/
