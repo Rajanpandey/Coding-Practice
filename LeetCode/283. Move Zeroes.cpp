0eba6bcefbec892a4c520c68b9e64d4f2881be8d
Rajan Pandey <rajanpandey2508@gmail.com>
2021-05-24 18:26:05 +0530

    https://leetcode.com/problems/move-zeroes/

--- LeetCode/283. Move Zeroes.cpp
+++ LeetCode/283. Move Zeroes.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        for(int slow = 0, fast = 0; fast < nums.size(); fast++) {
-            if(nums[fast] != 0) { 
-                swap(nums[slow++], nums[fast]); 
+        for (int slow = 0, fast = 0; fast < nums.size(); fast++) {
+            if (nums[fast] != 0) {
+                swap(nums[slow], nums[fast]);
+                slow++;
             }
         }
     }
