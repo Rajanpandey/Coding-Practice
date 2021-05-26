8a795317be100f55734116c0ed4eac6b8f7b0ee6
Rajan Pandey <rajanpandey2508@gmail.com>
2021-05-26 19:14:18 +0530

    https://leetcode.com/problems/reverse-linked-list/solution/

--- LeetCode/206. Reverse Linked List.cpp
+++ LeetCode/206. Reverse Linked List.cpp
@@ -3,24 +3,26 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* prev=NULL;
-        ListNode* next=NULL;
-        ListNode* curr=head;
-        
-        while(curr!=NULL){
-            next=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=next;
-        }   
-        head=prev;
+        if (head == NULL) return head;
+
+        ListNode* prev = NULL;
+        ListNode* next = head;
+        while (head->next) {
+            next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        head->next = prev;
+
         return head;
     }
-    
 };
