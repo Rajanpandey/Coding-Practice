225b5d47e1acda0c9721e83caa83b77a340b6109
Rajan Pandey <rajanpandey2508@gmail.com>
2021-05-31 16:19:45 +0530

    https://leetcode.com/problems/search-a-2d-matrix-ii/

--- LeetCode/240. Search a 2D Matrix II.rb
+++ LeetCode/240. Search a 2D Matrix II.rb
@@ -2,7 +2,7 @@
 # @param {Integer} target
 # @return {Boolean}
 def search_matrix(matrix, target)
-    matrix.each_with_index do |row, idx1|
+    matrix.each do |row|
         return false if (row[0] > target)
         return true if row.bsearch { |value| target <=> value }
     end
