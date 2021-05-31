2224cd52b59f72d5b15afd807b779d15ed495af8
Rajan Pandey <rajanpandey2508@gmail.com>
2021-05-31 15:51:43 +0530

    https://leetcode.com/problems/search-in-rotated-sorted-array/

--- LeetCode/162. Find Peak Element.cpp
+++ LeetCode/162. Find Peak Element.cpp
@@ -1,5 +1,40 @@
 class Solution {
 public:
+    int search(vector<int>& nums, int target) {
+        int lo = 0, hi = nums.size() - 1;
+
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[lo] <= nums[mid]) {
+                if (target < nums[mid] && nums[lo] <= target) {
+                    hi = mid;
+                } else {
+                    lo = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[hi]) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid;
+                }
+            }
+        }
+
+        return nums[lo] == target ? lo : -1;
+    }
+};
+
+
+/*
+Shorter solution:
+
+class Solution {
+public:
     int findPeakElement(vector<int>& nums) {
         int lo = 0, hi = nums.size() - 1;
 
@@ -16,3 +51,4 @@ public:
         return lo;
     }
 };
+*/
