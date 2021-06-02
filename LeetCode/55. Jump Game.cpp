90b1b17947235b907b900d455d2fad2b1f5fde8b
Rajan Pandey <rajanpandey2508@gmail.com>
2021-06-02 14:05:27 +0530

    https://leetcode.com/problems/jump-game/

--- LeetCode/55. Jump Game.cpp
+++ LeetCode/55. Jump Game.cpp
@@ -1,13 +1,40 @@
+// Greedy Solution: O(N) Time and O(1) Space Complexity
+
 class Solution {
 public:
-    bool canJump(vector<int>& A) {
+    bool canJump(vector<int>& nums) {
         int reach = 0;
-        for(int i=0; i<A.size(); i++){
-            if(i <= reach) 
-                reach = max(reach, i+A[i]);
-            else
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (i <= reach) {
+                reach = max(reach, i + nums[i]);
+            } else {
                 break;
+            }
         }
-        return reach >= A.size()-1;
+
+        return reach >= nums.size() - 1;
     }
 };
+
+
+/*
+Dynamic Programming Solution: O(N) Time and O(N) Space Complexity
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        if (nums[0] == 0 && nums.size() > 1) return false;
+
+        vector<int> dp = nums;
+
+        for (int i = 1; i < nums.size() - 1; i++) {
+            dp[i] = max(nums[i], dp[i - 1] - 1);
+
+            if (dp[i] == 0) return false;
+        }
+
+        return true;
+    }
+};
+*/
