7c7cf81cedf715748bd8976fe94c5ab72383e495
Rajan Pandey <rajanpandey2508@gmail.com>
2021-06-04 15:55:04 +0530

    https://leetcode.com/problems/roman-to-integer/

--- LeetCode/13. Roman to Integer.cpp
+++ LeetCode/13. Roman to Integer.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int ans = 0, curr = 0, next = 0;
-        map<char, int> romanValue = { {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000} };
+        int ans = 0;
+        int curr = 0, next = 0;
 
-        if(s.size() == 1) return romanValue[s[0]];
-        
-        for(int i=0; i<s.length()-1; i++) {
-            curr = romanValue[s[i]];
-            next = romanValue[s[i+1]];
-            ans += (next > curr) ? -curr : curr;
+        map<char, int> roman;
+        roman['I'] = 1;
+        roman['V'] = 5;
+        roman['X'] = 10;
+        roman['L'] = 50;
+        roman['C'] = 100;
+        roman['D'] = 500;
+        roman['M'] = 1000;
+
+        if (s.length() == 1) return roman[s[0]];
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            curr = roman[s[i]];
+            next = roman[s[i + 1]];
+
+            if (curr < next) {
+                ans -= curr;
+            } else {
+                ans += curr;
+            }
         }
+
         ans += next;
         
         return ans;
