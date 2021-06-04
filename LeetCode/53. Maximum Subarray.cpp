75d70fc829e29a715bf6cd890fefae1f4a799732
Rajan Pandey <rajanpandey2508@gmail.com>
2021-06-04 16:55:31 +0530

    https://leetcode.com/problems/maximum-subarray/

--- LeetCode/53. Maximum Subarray.cpp
+++ LeetCode/53. Maximum Subarray.cpp
@@ -1,12 +1,35 @@
+// Greedy Solution:
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int ans = INT_MIN, sum = 0;
-        for(auto i:nums) {
-            sum += i;
+        for (auto num : nums) {
+            sum += num;
             ans = max(sum, ans);
-            if(sum < 0) { sum=0; }
+            if (sum < 0) {
+                sum = 0;
+            }
         }
         return ans;
     }
 };
+
+/*
+DP Solution:
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        vector<int> dp (nums);
+        int ans = dp[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            dp[i] = max(dp[i - 1] + nums[i], nums[i]);
+            ans = max(ans, dp[i]);
+        }
+
+        return ans;
+    }
+};
+
+/*
