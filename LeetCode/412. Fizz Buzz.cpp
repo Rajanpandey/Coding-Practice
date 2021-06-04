fd34d9a104b9c4487b29d93b01e047bbe2dca173
Rajan Pandey <rajanpandey2508@gmail.com>
2021-06-04 14:37:18 +0530

    https://leetcode.com/problems/fizz-buzz

--- LeetCode/412. Fizz Buzz.cpp
+++ LeetCode/412. Fizz Buzz.cpp
@@ -18,3 +18,28 @@ public:
         return ans;
     }
 };
+
+
+/*
+C++ solution without if-else:
+
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> res(n);
+        for(int i = 1;i <= n; i++) {
+            res[i - 1] = to_string(i);
+        }
+        for(int i = 2;i < n; i += 3) {
+            res[i] = "Fizz";
+        }
+        for(int i = 4;i < n; i += 5) {
+            res[i] = "Buzz";
+        }
+        for(int i = 14;i < n; i += 15) {
+            res[i] = "FizzBuzz";
+        }
+        return res;
+    }
+};
+/*
