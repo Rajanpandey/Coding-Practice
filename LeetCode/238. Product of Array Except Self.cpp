b41c09f45af5196218aaf2bfcccc9a42c83ab19d
Rajan Pandey <rajanpandey2508@gmail.com>
2021-06-05 19:23:10 +0530

    update LeetCode stats in README

--- LeetCode/238. Product of Array Except Self.cpp
+++ LeetCode/238. Product of Array Except Self.cpp
@@ -11,3 +11,35 @@ public:
         return ans;
     }
 };
+
+/*
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int totalProduct = 1, zeroCount = 0;
+        vector<int> ans(nums.size(), 0);
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i]) {
+                totalProduct *= nums[i];
+            } else {
+                zeroCount++;
+            }
+        }
+
+        if (zeroCount > 1) {
+            return ans;
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i]) {
+                ans[i] = zeroCount ? 0 : totalProduct / nums[i];
+            } else {
+                ans[i] = totalProduct;
+            }
+        }
+
+        return ans;
+    }
+};
+/*
