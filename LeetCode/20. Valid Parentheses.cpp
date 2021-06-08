89402efd02bdecc14277014194164ab2883804fe
Rajan Pandey <rajanpandey2508@gmail.com>
2021-06-08 18:36:15 +0530

    https://leetcode.com/problems/valid-parentheses/

--- LeetCode/20. Valid Parentheses.cpp
+++ LeetCode/20. Valid Parentheses.cpp
@@ -14,3 +14,31 @@ public:
         return myStack.empty();
     }
 };
+
+
+/*
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> myStack;
+        map<char, char> myMap;
+        myMap[')'] = '(';
+        myMap['}'] = '{';
+        myMap[']'] = '[';
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                myStack.push(s[i]);
+            } else {
+                if (myStack.top() == myMap[s[i]]) {
+                    myStack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return myStack.empty();
+    }
+};
+/*
