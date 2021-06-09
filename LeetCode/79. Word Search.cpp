70457b9280850e411431e663da7107a7db352bf9
Rajan Pandey <rajanpandey2508@gmail.com>
2021-06-09 18:22:07 +0530

    https://leetcode.com/problems/word-search/

--- LeetCode/79. Word Search.cpp
+++ LeetCode/79. Word Search.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int n, m;
     
-    bool game(vector<vector<char>>& board, int i, int j, string word, int k) {
+    bool DSFBacktracking(vector<vector<char>>& board, int i, int j, string word, int k) {
         if (i < 0 || j < 0 || i >= n || j >= m || board[i][j] != word[k]) return false;
         
         if (k == word.size() - 1) return true;
@@ -10,10 +10,10 @@ public:
         char curr = board[i][j];
         board[i][j] = '*';
         
-        bool searchNext = game(board, i, j + 1, word, k + 1)
-                       || game(board, i, j - 1, word, k + 1)
-                       || game(board, i + 1, j, word, k + 1)
-                       || game(board, i - 1, j, word, k + 1);
+        bool searchNext = DSFBacktracking(board, i, j + 1, word, k + 1)
+                       || DSFBacktracking(board, i, j - 1, word, k + 1)
+                       || DSFBacktracking(board, i + 1, j, word, k + 1)
+                       || DSFBacktracking(board, i - 1, j, word, k + 1);
 
         board[i][j] = curr;
         
@@ -25,7 +25,7 @@ public:
         
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (game(board, i, j, word, 0)) {
+                if (DSFBacktracking(board, i, j, word, 0)) {
                     return true;
                 }
             }
