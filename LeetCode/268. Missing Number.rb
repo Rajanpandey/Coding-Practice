b5a7a4e6fec5ae6715adc8ace12ad36a6d9edbe4
Rajan Pandey <rajanpandey2508@gmail.com>
2021-06-10 13:25:58 +0530

    https://leetcode.com/problems/missing-number

--- LeetCode/268. Missing Number.rb
+++ LeetCode/268. Missing Number.rb
@@ -1,5 +1,5 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def missing_number(nums)
-    return (1..(nums.size)).reduce(:+) - nums.reduce(:+)
+    (1..(nums.size)).reduce(:+) - nums.reduce(:+)
 end
