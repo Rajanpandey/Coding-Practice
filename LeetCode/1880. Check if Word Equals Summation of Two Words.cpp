8913bf3ea29e799c3a71ee86252d7d0f6b094f2b
Rajan Pandey <rajanpandey2508@gmail.com>
2021-06-12 12:03:25 +0530

    https://leetcode.com/problems/check-if-word-equals-summation-of-two-words/

--- LeetCode/1880. Check if Word Equals Summation of Two Words.cpp
+++ LeetCode/1880. Check if Word Equals Summation of Two Words.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool isSumEqual(string firstWord, string secondWord, string targetWord) {
         int first = 0, second = 0, third = 0;
+
         for (auto ch : firstWord) {
             first = (first * 10) + (ch - 'a');
         }
