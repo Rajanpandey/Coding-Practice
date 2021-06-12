37bcdb10a5d1f332287b07ce15cba3410c2ac1ff
Rajan Pandey <rajanpandey2508@gmail.com>
2021-06-12 12:03:01 +0530

    https://leetcode.com/problems/running-sum-of-1d-array/

---
+++ LeetCode/1880. Check if Word Equals Summation of Two Words.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isSumEqual(string firstWord, string secondWord, string targetWord) {
+        int first = 0, second = 0, third = 0;
+        for (auto ch : firstWord) {
+            first = (first * 10) + (ch - 'a');
+        }
+        for (auto ch : secondWord) {
+            second = (second * 10) + (ch - 'a');
+        }
+        for (auto ch : targetWord) {
+            third = (third * 10) + (ch - 'a');
+        }
+        
+        return (first + second) == third;
+    }
+};
