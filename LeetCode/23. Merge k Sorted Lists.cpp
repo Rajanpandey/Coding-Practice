1cb07add6d852514a3cf45038fb4f8d40db47c01
Rajan Pandey <rajanpandey2508@gmail.com>
2021-06-16 17:34:12 +0530

    https://leetcode.com/problems/merge-k-sorted-lists/submissions/

--- LeetCode/23. Merge k Sorted Lists.cpp
+++ LeetCode/23. Merge k Sorted Lists.cpp
@@ -11,6 +11,36 @@
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (lists.empty()) return NULL;
+
+        while (lists.size() > 1) {
+            lists.push_back(mergeTwoLists(lists[0], lists[1]));
+            lists.erase(lists.begin());
+            lists.erase(lists.begin());
+        }
+
+        return lists[0];
+    }
+
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (!l1) return l2;
+        if (!l2) return l1;
+
+        if (l1->val <= l2->val) {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
+        }
+    }
+};
+
+/*
+Using priority queue:
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
         priority_queue <int, vector<int>, greater<int>> q;
         
         for (auto list : lists) {
@@ -34,3 +64,4 @@ public:
         return ans;
     }
 };
+/*
