7255e7038b1d87a6202f752b8a419f0072a448ec
Rajan Pandey <rajanpandey2508@gmail.com>
2021-06-18 17:41:58 +0530

    https://leetcode.com/problems/house-robber-ii/

--- LeetCode/213. House Robber II.cpp
+++ LeetCode/213. House Robber II.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    int robber(vector<int>& nums) {
-        int n = nums.size();
-        
-        vector <int> dp = nums;
+    int robber(vector<int>& dp) {
+        int n = dp.size();
+
         dp[2] += dp[0];
-        
+
         for (int i = 3; i < n; i++) {
             dp[i] += max(dp[i - 2], dp[i - 3]);
         }
-        
+
         return max(dp[n - 1], dp[n - 2]);
     }
-    
+
     int rob(vector<int>& nums) {
         if (nums.size() <= 3) return *max_element(nums.begin(), nums.end());
-        
-        vector<int> vec1(nums.begin(), nums.end() - 1);
-        vector<int> vec2(nums.begin() + 1, nums.end());
-        
-        return max(robber(vec1), robber(vec2));
+
+        vector<int> dp1(nums.begin(), nums.end() - 1);
+        vector<int> dp2(nums.begin() + 1, nums.end());
+
+        return max(robber(dp1), robber(dp2));
     }
 };
