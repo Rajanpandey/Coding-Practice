53159ea0abf755437115fd6a1050df14397d2b72
Rajan Pandey <rajanpandey2508@gmail.com>
2021-06-21 15:01:47 +0530

    https://leetcode.com/problems/counting-bits/

--- LeetCode/338. Counting Bits.cpp
+++ LeetCode/338. Counting Bits.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
+        vector<int> ans = {0};
+
+        for (int i = 1; i < n + 1; i++) {
+            ans.push_back(ans[i >> 1] + (i & 1));
+        }
+
+        return ans;
+    }
+};
+
+/*
+Another solution:
+class Solution {
+public:
+    vector<int> countBits(int n) {
         vector<int> ans(n + 1);
         
         for (int i = 1; i < n + 1; i++) {
@@ -10,3 +25,4 @@ public:
         return ans;
     }
 };
+/*
