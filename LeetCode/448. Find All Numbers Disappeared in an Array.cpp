e18d35f11b282e438ba9d02f073047d3c142714d
Rajan Pandey <rajanpandey2508@gmail.com>
2021-06-22 17:31:02 +0530

    https://leetcode.com/problems/range-sum-query-immutable/

--- LeetCode/448. Find All Numbers Disappeared in an Array.cpp
+++ LeetCode/448. Find All Numbers Disappeared in an Array.cpp
@@ -24,6 +24,24 @@ public:
 };
 
 /*
+Same solution but shorter:
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> ans;
+
+        for (int i = 0; i < nums.size(); i++)
+            if (nums[abs(nums[i]) - 1] > 0)
+                nums[abs(nums[i]) - 1] *= -1;
+
+        for (int i = 0; i < nums.size(); i++)
+            if (nums[i] > 0)
+                ans.push_back(i + 1);
+
+        return ans;
+    }
+};
+
 O(N) Space solution:
 
 class Solution {
