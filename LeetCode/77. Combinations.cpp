477d8943bc1ff8aee4ec368dddfccd1e9cfba2c0
Rajan Pandey <rajanpandey2508@gmail.com>
2021-06-29 19:26:05 +0530

    https://leetcode.com/problems/combinations/

---
+++ LeetCode/77. Combinations.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void backtracking(int start, int end, int k, vector<vector<int>>& ans, vector<int> arr) {
+        if (arr.size() == k) {
+            ans.push_back(arr);
+        }
+        
+        for (int i = start; i <= end; i++) {
+            arr.push_back(i);
+            backtracking(i + 1, end, k, ans, arr);
+            arr.pop_back();
+        }
+    }
+    
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> ans;
+        backtracking(1, n, k, ans, {});
+        return ans;
+    }
+};
