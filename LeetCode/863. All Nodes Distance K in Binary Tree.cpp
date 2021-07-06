df57d374d8a38b1d11bd1b6a74f652066f92aa3b
Rajan Pandey <rajanpandey2508@gmail.com>
2021-07-06 16:52:13 +0530

    https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/

--- LeetCode/863. All Nodes Distance K in Binary Tree.cpp
+++ LeetCode/863. All Nodes Distance K in Binary Tree.cpp
@@ -7,6 +7,60 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+// Recursive Solution:
+class Solution {
+public:
+    void DFS(TreeNode* node, int k, map<TreeNode*, TreeNode*> childToParentMap, map<TreeNode*, bool>& seen, vector<int>& ans) {
+        if (seen[node]) return;
+
+        if (k == 0) {
+            ans.push_back(node->val);
+            return;
+        }
+
+        seen[node] = true;
+
+        if (node->left) {
+            DFS(node->left, k - 1, childToParentMap, seen, ans);
+        }
+
+        if (node->right) {
+            DFS(node->right, k - 1, childToParentMap, seen, ans);
+        }
+
+        if (childToParentMap[node]) {
+            DFS(childToParentMap[node], k - 1, childToParentMap, seen, ans);
+        }
+    }
+
+    void mapChildToParent(TreeNode* root, map<TreeNode*, TreeNode*>& childToParentMap) {
+        if (!root) return;
+
+        if (root->left) childToParentMap[root->left] = root;
+        if (root->right) childToParentMap[root->right] = root;
+
+        mapChildToParent(root->left, childToParentMap);
+        mapChildToParent(root->right, childToParentMap);
+    }
+
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        vector<int> ans;
+        if (k == 0) return {target->val};
+
+        map<TreeNode*, TreeNode*> childToParentMap;
+        mapChildToParent(root, childToParentMap);
+
+        map<TreeNode*, bool> seen;
+
+        DFS(target, k, childToParentMap, seen, ans);
+
+        return ans;
+    }
+};
+
+
+/* Iterative Solution with same concept:
+
 class Solution {
 public:
     void mapChildToParent(TreeNode* root, map<TreeNode*, TreeNode*>& childToParentMap) {
@@ -63,3 +117,5 @@ public:
         return ans;
     }
 };
+
+*/
