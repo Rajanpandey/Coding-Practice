25cca484925d7592a493f7ee0a1d7421f434cf9e
Rajan Pandey <rajanpandey2508@gmail.com>
2021-07-06 16:33:18 +0530

    https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/

---
+++ LeetCode/863. All Nodes Distance K in Binary Tree.cpp
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void mapChildToParent(TreeNode* root, map<TreeNode*, TreeNode*>& childToParentMap) {
+        if (!root) return;
+        
+        if (root->left) childToParentMap[root->left] = root;
+        if (root->right) childToParentMap[root->right] = root;
+        
+        mapChildToParent(root->left, childToParentMap);
+        mapChildToParent(root->right, childToParentMap);
+    }
+    
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        vector<int> ans;
+        if (k == 0) return {target->val};
+        
+        map<TreeNode*, TreeNode*> childToParentMap;
+        mapChildToParent(root, childToParentMap);
+        
+        map<TreeNode*, bool> seen;
+        queue<TreeNode*> q;
+        int distance = 0;
+        
+        while (distance != k) {
+            q.push(target);
+            seen[target] = true;
+            
+            int qSize = q.size();
+            for (int i = 0; i < qSize; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                
+                if (node->left && !seen[node->left]) {
+                    q.push(node->left);
+                    seen[node->left] = true;
+                }
+                if (node->right && !seen[node->right]) {
+                    q.push(node->right);
+                    seen[node->right] = true;
+                }
+                if (childToParentMap[node] && !seen[childToParentMap[node]]) {
+                    q.push(childToParentMap[node]);
+                    seen[childToParentMap[node]] = true;
+                }
+            }
+            distance++;
+        }
+        
+        while (!q.empty()) {
+            ans.push_back(q.front()->val);
+            q.pop();
+        }
+        
+        return ans;
+    }
+};
