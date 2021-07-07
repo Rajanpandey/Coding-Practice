a5d82544d996889c6285220552e4cbc766e6e54a
Rajan Pandey <rajanpandey2508@gmail.com>
2021-07-07 21:36:28 +0530

    https://leetcode.com/problems/course-schedule/

---
+++ LeetCode/207. Course Schedule.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> adjList[numCourses];
+        vector<int> indegree(numCourses, 0);
+        
+        for (auto p : prerequisites) {
+            adjList[p[1]].push_back(p[0]);
+            indegree[p[0]]++;
+        }
+
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        while (!q.empty()) {
+            int ele = q.front();
+            q.pop();
+            numCourses--;
+            for (auto child : adjList[ele]) {
+                indegree[child]--;
+                if(indegree[child] == 0) {
+                    q.push(child);
+                }
+            }
+        }
+        return numCourses == 0;
+    }
+};
