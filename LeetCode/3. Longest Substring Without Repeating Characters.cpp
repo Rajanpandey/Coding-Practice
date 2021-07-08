7e9ea263c30384bdf02424279dd41398efd5c79b
Rajan Pandey <rajanpandey2508@gmail.com>
2021-07-08 15:37:31 +0530

    https://leetcode.com/problems/longest-substring-without-repeating-characters/

--- LeetCode/3. Longest Substring Without Repeating Characters.cpp
+++ LeetCode/3. Longest Substring Without Repeating Characters.cpp
@@ -1,3 +1,28 @@
+// Sliding Window Solution:
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        set<char> mySet;
+        int fast = 0, slow = 0, n = s.size(), ans = 0;
+
+        while (slow < n && fast < n) {
+            if (mySet.find(s[fast]) != mySet.end()) {
+                mySet.erase(s[slow]);
+                slow++;
+            } else {
+                mySet.insert(s[fast]);
+                ans = max(ans, fast - slow + 1);
+                fast++;
+            }
+        }
+
+        return ans;
+    }
+};
+
+/*
+Without Sliding Window:
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
@@ -22,27 +47,4 @@ public:
     }
 };
 
-
-/*
-Sliding Window Solution:
-class Solution {
-public:
-    int lengthOfLongestSubstring(string s) {
-        set<char> mySet;
-        int fast = 0, slow = 0, n = s.size(), ans = 0;
-
-        while(fast < n && slow < n) {
-            // Not found in mySet
-            if (mySet.find(s[fast]) == mySet.end()) {
-                mySet.insert(s[fast]);
-                fast++;
-                ans = max(ans, fast - slow);
-            } else {
-                mySet.erase(s[slow]);
-                slow++;
-            }
-        }
-        return ans;
-    }
-};
 /*
