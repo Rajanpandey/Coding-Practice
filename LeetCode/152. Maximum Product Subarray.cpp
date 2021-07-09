dd6b83e7f3c0283c1b634493467559a9fd7ea323
Rajan Pandey <rajanpandey2508@gmail.com>
2021-07-09 11:46:34 +0530

    https://leetcode.com/problems/maximum-product-subarray/

--- LeetCode/152. Maximum Product Subarray.cpp
+++ LeetCode/152. Maximum Product Subarray.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        if (nums.empty()) return 0;
-        
         int ans = INT_MIN;
-        
         int maxProd = 1, minProd = 1;
-        
+
         for (int num : nums) {
             if (num < 0) {
                 swap(maxProd, minProd);
             }
-            
+
             maxProd = max(maxProd * num, num);
             minProd = min(minProd * num, num);
 
             ans = max(ans, maxProd);
         }
-        
+
         return ans;
     }
 };
