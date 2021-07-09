43a8128bf924bd889162ac683078a6dd55121ad7
Rajan Pandey <rajanpandey2508@gmail.com>
2021-07-09 14:52:14 +0530

    https://leetcode.com/problems/maximal-square/

--- LeetCode/221. Maximal Square.cpp
+++ LeetCode/221. Maximal Square.cpp
@@ -5,15 +5,15 @@ public:
         vector<vector<int> > dp(n+1, vector<int>(m+1));
         
         int maxSquareSide = 0;
-        for(int i=1; i<=n; i++) {
-            for(int j=1; j<=m; j++) {
-                if(matrix[i-1][j-1] == '1') {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (matrix[i-1][j-1] == '1') {
                     dp[i][j] = min( min(dp[i][j-1], dp[i-1][j]), dp[i-1][j-1] ) + 1;
                     maxSquareSide = max(maxSquareSide, dp[i][j]);
                 }
             }
         }
-        
+
         return maxSquareSide*maxSquareSide;
     }
 };
