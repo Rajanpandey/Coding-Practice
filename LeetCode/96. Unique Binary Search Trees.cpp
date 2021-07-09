ccdf68693bce8b8a2e595fa9f15c07f58eac2dd9
Rajan Pandey <rajanpandey2508@gmail.com>
2021-07-09 12:47:57 +0530

    https://leetcode.com/problems/unique-binary-search-trees/

--- LeetCode/96. Unique Binary Search Trees.cpp
+++ LeetCode/96. Unique Binary Search Trees.cpp
@@ -5,6 +5,14 @@ public:
         dp[0] = 1;
         dp[1] = 1;
 
+        /* Catalan Number
+            F(n) = F(0) * F(n-1)
+                 + F(1) * F(n-2)
+                 + F(2) * F(n-3)
+                 + ...
+                 + F(n-2) * F(1)
+                 + F(n-1) * F(0)
+        */
         for (int i = 2; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 dp[i] += dp[j - 1] * dp[i - j];
