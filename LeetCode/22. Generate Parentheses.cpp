414a3dae18d43172504a129168437b6fa8266abd
Rajan Pandey <rajanpandey2508@gmail.com>
2021-07-09 10:52:42 +0530

    https://leetcode.com/problems/generate-parentheses/

--- LeetCode/22. Generate Parentheses.cpp
+++ LeetCode/22. Generate Parentheses.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     void backtrack(vector<string> &ans, string str, int open, int close, int n) {
-        if (open < close) {
-            return;
-        }
-        
-        if (open == close && open == n) {
+        if (open == n && close == n) {
             ans.push_back(str);
             return;
         }
-        
+
         if (open < n) {
-            backtrack(ans, str + "(", open + 1, close, n);
+            backtrack(ans, str + '(', open + 1, close, n);
         }
-        
-        if (close < n) {
-            backtrack(ans, str + ")", open, close + 1, n);
+
+        if (close < n && close < open) {
+            backtrack(ans, str + ')', open, close + 1, n);
         }
     }
-    
+
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
         backtrack(ans, "", 0, 0, n);
