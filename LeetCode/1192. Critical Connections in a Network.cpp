1c7b02bf21720d5e714386135c86b59d2a2d1241
Rajan Pandey <rajanpandey2508@gmail.com>
2021-07-11 22:32:36 +0530

    https://leetcode.com/problems/critical-connections-in-a-network/

--- LeetCode/1192. Critical Connections in a Network.cpp
+++ LeetCode/1192. Critical Connections in a Network.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     // Tarjan's Algorithm to find Strongly Connected Components
-    void DFS(int curr, int prev, vector<vector<int>>& ans, map<int, vector<int>>& adjList, vector<int>& disc, vector<int>& low, int time) {
-        disc[curr] = low[curr] = time++;
+    void DFS(int curr, int prev, vector<vector<int>>& ans, map<int, vector<int>>& adjList, vector<int>& discovered, vector<int>& low, int time) {
+        discovered[curr] = low[curr] = time++;
 
         for (int next : adjList[curr]) {
-            if (disc[next] == 0) {
-                DFS(next, curr, ans, adjList, disc, low, time);
+            if (discovered[next] == 0) {
+                DFS(next, curr, ans, adjList, discovered, low, time);
                 low[curr] = min(low[curr], low[next]);
             } else if (next != prev) {
-                low[curr] = min(low[curr], disc[next]);
+                low[curr] = min(low[curr], discovered[next]);
             }
 
-            if (low[next] > disc[curr]) {
+            if (low[next] > discovered[curr]) {
                 ans.push_back({curr, next});
             }
         }
@@ -21,14 +21,14 @@ public:
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
         vector<vector<int>> ans;
         map<int, vector<int>> adjList;
-        vector<int> disc(n), low(n);
+        vector<int> discovered(n), low(n);
         int time = 1;
 
         for (auto conn : connections) {
             adjList[conn[0]].push_back(conn[1]);
             adjList[conn[1]].push_back(conn[0]);
         }
-        DFS(0, -1, ans, adjList, disc, low, time);
+        DFS(0, -1, ans, adjList, discovered, low, time);
 
         return ans;
     }
