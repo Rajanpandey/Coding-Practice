fa34479a030f8f5d0d0c6d8e35e5d71260ff16e2
Rajan Pandey <rajanpandey2508@gmail.com>
2021-07-12 12:55:32 +0530

    https://leetcode.com/problems/course-schedule-ii/

--- LeetCode/210. Course Schedule II.cpp
+++ LeetCode/210. Course Schedule II.cpp
@@ -1,28 +1,29 @@
+// Kahn's Algorithm for Topological Sort
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<int> ans;
         vector<vector<int>> adjList(numCourses);
-        vector<int> indegree(numCourses, 0);
-        
+        vector<int> indegree(numCourses);
+
         for (auto p : prerequisites) {
             adjList[p[1]].push_back(p[0]);
             indegree[p[0]]++;
         }
-        
+
         queue<int> q;
-        for (int i = 0; i < numCourses; i++) {
+        for (int i = 0; i < indegree.size(); i++) {
             if (indegree[i] == 0) {
                 q.push(i);
             }
         }
-        
+
         while (!q.empty()) {
             int preReq = q.front();
             q.pop();
+
             ans.push_back(preReq);
-            numCourses--;
-            
+
             for (auto course : adjList[preReq]) {
                 indegree[course]--;
                 if (indegree[course] == 0) {
@@ -30,10 +31,11 @@ public:
                 }
             }
         }
-        
-        if (numCourses == 0)
+
+        if (numCourses == ans.size()) {
             return ans;
-        else 
+        } else {
             return {};
+        }
     }
 };
