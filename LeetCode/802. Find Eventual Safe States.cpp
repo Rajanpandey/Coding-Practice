d3c1fb09cddb3deafa02a38ef3231c4e053b2eae
Rajan Pandey <rajanpandey2508@gmail.com>
2021-07-12 13:16:19 +0530

    https://leetcode.com/problems/find-eventual-safe-states/

--- LeetCode/802. Find Eventual Safe States.cpp
+++ LeetCode/802. Find Eventual Safe States.cpp
@@ -1,3 +1,39 @@
+// Graph Coloring Algorithm
+class Solution {
+public:
+    bool DFS(vector<vector<int>> & graph, vector<int> & color, int i) {
+        if (color[i]) {
+            return color[i] == 1;
+        }
+
+        color[i] = -1;
+
+        for(int v : graph[i]) {
+            if(!DFS(graph, color, v)) {
+                return false;
+            }
+        }
+
+        color[i] = 1;
+        return true;
+    }
+
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        vector<int> ans;
+        vector<int> color(graph.size());
+
+        for (int i = 0; i < graph.size(); ++i) {
+            if (DFS(graph, color, i)) {
+                ans.push_back(i);
+            }
+        }
+
+        return ans;
+    }
+};
+
+/* DFS Sol:
+
 class Solution {
 public:
     unordered_set<int> visitedNodes;
@@ -38,3 +74,5 @@ public:
         return ans;        
     }
 };
+
+/*
