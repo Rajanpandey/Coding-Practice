468d422d529c8a1afc8021188d9e3fd0f8823206
Rajan Pandey <rajanpandey2508@gmail.com>
2021-07-12 16:55:23 +0530

    Revert "Dijkstra"
    This reverts commit 7acc487b4c22467ef139dcbb0937b12a7b12ddab.

--- LeetCode/743. Network Delay Time.cpp
+++ LeetCode/743. Network Delay Time.cpp
@@ -1,4 +1,4 @@
-// Dijkstra's Algorithm to find Single Source Shortest Path
+// Djikstra's Algorithm to find Single Source Shortest Path
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
