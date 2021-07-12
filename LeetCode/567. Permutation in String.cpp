149c116b31cffbb184c27c8446c0497134376565
Rajan Pandey <rajanpandey2508@gmail.com>
2021-07-12 19:30:20 +0530

    https://leetcode.com/problems/permutation-in-string/

--- LeetCode/567. Permutation in String.cpp
+++ LeetCode/567. Permutation in String.cpp
@@ -2,26 +2,22 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         int n = s1.length(), m = s2.length();
-        if (n > m) return false;
         vector<int> freq1(26), freq2(26);
-        
-        for (auto ch : s1) {
-            freq1[ch - 'a']++;
-        }
-        
+
+        if (n > m) return false;
+
         for (int i = 0; i < n; i++) {
+            freq1[s1[i] - 'a']++;
             freq2[s2[i] - 'a']++;
         }
-        
+
         int start = 0, end = n - 1;
-        while (start < m && end < m) {
+        while (end < m) {
             if (freq1 == freq2) {
                 return true;
             }
-            
-            if (freq2[s2[start] - 'a'] > 0) {
-                freq2[s2[start] - 'a']--;
-            }
+
+            freq2[s2[start] - 'a']--;
             start++;
             end++;
             if (end < m) {
