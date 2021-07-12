9a96ae844391bc795c02370132e3556291fe7315
Rajan Pandey <rajanpandey2508@gmail.com>
2021-07-12 14:53:29 +0530

    https://leetcode.com/problems/reconstruct-itinerary/

--- LeetCode/332. Reconstruct Itinerary.cpp
+++ LeetCode/332. Reconstruct Itinerary.cpp
@@ -1,29 +1,28 @@
 // Hierholzer's Algorithm to find Eulers's Path
 class Solution {
 public:
-    void DFS(string src, map<string, vector<string>>& adjList, vector<string>& ans) {
-        while (!adjList[src].empty()) {
-            string s = adjList[src].back();
-            adjList[src].pop_back();
-            DFS(s, adjList, ans);
+    void DFS(string source, map<string, priority_queue<string, vector<string>, greater<string>>>& adjList, vector<string>& ans) {
+        while (!adjList[source].empty()) {
+            string destination = adjList[source].top();
+            adjList[source].pop();
+            DFS(destination, adjList, ans);
         }
-        ans.push_back(src);
+
+        ans.push_back(source);
     }
-    
+
     vector<string> findItinerary(vector<vector<string>>& tickets) {
-        map<string, vector<string>> adjList;
-        
-        for (auto ticket : tickets) {
-            adjList[ticket[0]].push_back(ticket[1]);
-        }
-        
-        for (auto &ticket : adjList) {
-            sort(ticket.second.begin(), ticket.second.end(), greater<string>());
-        }
-        
         vector<string> ans;
+        map<string, priority_queue<string, vector<string>, greater<string>>> adjList;
+
+        for (auto ticket : tickets) {
+            adjList[ticket[0]].push(ticket[1]);
+        }
+
         DFS("JFK", adjList, ans);
+
         reverse(ans.begin(), ans.end());
+
         return ans;
     }
 };
