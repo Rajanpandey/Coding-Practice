1478ea5bf930ca3898c9d042933d115c85c7bab5
Rajan Pandey <rajanpandey2508@gmail.com>
2021-09-02 11:47:10 +0530

    https://www.interviewbit.com/problems/anagrams/

--- InterviewBit/Hashing/Anagrams.cpp
+++ InterviewBit/Hashing/Anagrams.cpp
@@ -1,17 +1,15 @@
 vector<vector<int> > Solution::anagrams(const vector<string> &A) {
-    unordered_map<string, vector<int>> myMap;
+    map<string, vector<int>> myMap;
     vector<vector<int>> ans;
-    
-    for(int i=0; i<A.size(); i++) {
+
+    for (int i = 0; i < A.size(); i++) {
         string temp = A[i];
         sort(temp.begin(), temp.end());
-        myMap[temp].push_back(i+1);
+        myMap[temp].push_back(i + 1);
     }
-    
-    auto it = myMap.begin();
-    while(it != myMap.end()){
-        ans.push_back(it->second);
-        it++;
+
+    for (auto it : myMap) {
+        ans.push_back(it.second);
     }
     
     return ans;
