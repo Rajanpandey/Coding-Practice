f06ab61edfd87d451887d53ba759f699eee70b95
Rajan Pandey <rajanpandey2508@gmail.com>
2021-09-04 13:48:52 +0530

    https://www.hackerrank.com/challenges/new-year-chaos/

--- HackerRank/Algorithms/Constructive Algorithms/New Year Chaos.cpp
+++ HackerRank/Algorithms/Constructive Algorithms/New Year Chaos.cpp
@@ -1,14 +1,21 @@
-void minimumBribes(vector<int> q) {    
-    int ans = 0;
+void minimumBribes(vector<int> q) {
+    int count = 0, n = q.size();
 
-    for(int i=q.size()-1; i>=0; i--) {
-        if(q[i]-1-i > 2) {
-            cout << "Too chaotic" << endl; return;
+    for (int i = n - 1; i >= 0; i--) {
+        if (q[i] != (i + 1)) {
+            if (q[i - 1] == (i + 1)) {
+                count++;
+                swap(q[i], q[i - 1]);
+            } else if (q[i - 2] == (i + 1)) {
+                count += 2;
+                swap(q[i - 2], q[i - 1]);
+                swap(q[i - 1], q[i]);
+            } else {
+                cout << "Too chaotic" << endl;
+                return;
+            }
         }
-
-        for(int j=max(0, q[i]-2); j<i; j++)
-            if(q[j] > q[i]) ans++;
     }
-    
-    cout << ans << endl;
+
+    cout << count << endl;
 }
