98379a8ac44a440ce9a9ddb03d0271af654be825
Rajan Pandey <rajanpandey2508@gmail.com>
2021-09-04 12:52:11 +0530

    https://www.hackerrank.com/challenges/ctci-array-left-rotation/

--- HackerRank/Data Structures/Arrays/Left Rotation.cpp
+++ HackerRank/Data Structures/Arrays/Left Rotation.cpp
@@ -1,9 +1,12 @@
-vector<int> rotLeft(vector<int> a, int d) {
-    int n = a.size();
-    vector<int> ans(n);
+vector<int> rotLeft(vector<int> a, int shiftAmount) {
+    int lengthOfArray = a.size();
+    vector<int> ans(lengthOfArray);
 
-    for (int i = 0; i < n; i++) {
-        int newLocation = (n + i - d) % n;
+    for (int i = 0; i < lengthOfArray; i++) {
+        // For left rotation,  int newLocation = (i + (lengthOfArray - shiftAmount)) % lengthOfArray;
+        // For right rotation, int newLocation = (i + (lengthOfArray + shiftAmount)) % lengthOfArray;
+        // Or, int newLocation = (i + shiftAmount) % lengthOfArray;
+        int newLocation = (i + (lengthOfArray - shiftAmount)) % lengthOfArray;
         ans[newLocation] = a[i];
     }
 
