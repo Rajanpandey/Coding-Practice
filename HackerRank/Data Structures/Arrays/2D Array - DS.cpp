fa36d984c11c8f7fef198c9084f78f48f9f2f123
Rajan Pandey <rajanpandey2508@gmail.com>
2021-09-04 12:35:43 +0530

    https://www.interviewbit.com/problems/anagrams/

--- HackerRank/Data Structures/Arrays/2D Array - DS.cpp
+++ HackerRank/Data Structures/Arrays/2D Array - DS.cpp
@@ -1,10 +1,12 @@
 int hourglassSum(vector<vector<int>> arr) {
-    int ans = INT_MIN, sum = 0;
-    for(int i=0; i<4; i++) {        
-        for(int j=0; j<4; j++) {
-            sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
-            ans = max(ans, sum);
+    int ans = INT_MIN, currSum;
+
+    for (int i = 0; i < arr.size() - 2; i++) {
+        for (int j = 0; j < arr[0].size() - 2; j++) {
+            currSum = (arr[i][j] + arr[i][j + 1] + arr[i][j + 2]) + (arr[i + 1][j + 1]) + (arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]);
+            ans = max(ans, currSum);
         }
     }
+
     return ans;
 }
